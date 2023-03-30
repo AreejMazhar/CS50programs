b85fd0071f264f6afc9419386884bda0e53771ae
Areej Mazhar <73883627+AreejMazhar@users.noreply.github.com>
2023-03-30 12:16:00 +0500

    Create Week 2 - Mario less.c

---
+++ #include <cs50.h> #include <stdio.h>  int main(void) {/user input for positive integer between 1 to 8 inclusive      int height;     do     {         height = get_int("Height: ");     }     while (height < 1 || height > 8);/making left side of the pyramid/Week 2 - Mario less.c
@@ -0,0 +1,37 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    //user input for positive integer between 1 to 8 inclusive
+
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    //making left side of the pyramid
+    //need rows and columns
+
+    int row;
+    int column;
+    int space;
+
+    for (row = 0; row < height; row++)
+    {
+        for (space = 0; space < height - row - 1; space++)
+        {
+            printf(" ");
+        }
+
+        for (column = 0; column <= row; column++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+
+}
